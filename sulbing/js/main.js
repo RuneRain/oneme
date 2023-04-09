99278465792ec0bf1467957a9d40af7396418883
oneme <128768823+RuneRain@users.noreply.github.com>
2023-04-10 00:42:27 +0900

    Update main.js

--- sulbing/js/main.js
+++ sulbing/js/main.js
@@ -1,3 +1,4 @@
+/* menu slider */
 
 $('nav').mouseenter(function(){
   $('.gnbSubWrap').stop().slideDown(500);
@@ -7,21 +8,61 @@ $('nav').mouseleave(function(){
   })
 
 
-  const sildes = document.querySelector('#slider>ul'); //ul 슬라이드 컨테이너
-  const sildeImg = document.querySelectorAll('#slider>ul li'); //슬라이드들
-  const imgWidth = sildeImg[0].children[0].offsetWidth; //이미지 가로길이(css적용 후)
-  const slideCount =  sildeImg.length;  //슬라이드 개수
-  const prev = document.querySelector('.prev') //이전버튼
-  const next = document.querySelector('.next') //다음버튼
-  
-  sildes.style.width = `${imgWidth*(slideCount+2)}px`
-  
-  function makeClone(){
-    let clone_first = sildeImg[0].cloneNode(true);
-    let clone_last = sildes.lastElementChild.cloneNode(true);
-    sildes.append(clone_first);
-    sildes.firstElementChild.before(clone_last);
-  
-    console.log('clone_last',clone_first)
-  }
-  makeClone();
+/* main slider */
+$(function(){
+  const mainImgWidth = $('.slider img').width();
+  const mainImgNember = $('.slider img').length;
+  const mainSliderUl = $('.slider > ul');
+
+  mainSliderUl.css({width:mainImgWidth*mainImgNember})
+
+  setInterval(function(){
+    mainSliderUl.animate({'margin-left':-mainImgWidth},300, 'swing', function(){
+      mainSliderUl.css({'margin-left':0})
+      mainSliderUl.find('li:first-child').insertAfter(mainSliderUl.find('li:last-child')); 
+    });
+  },3000)
+
+const right = function(){
+  mainSliderUl.animate({'margin-left':-mainImgWidth},300, 'swing', function(){
+    mainSliderUl.css({'margin-left':0})
+    mainSliderUl.find('li:first-child').insertAfter(mainSliderUl.find('li:last-child')); 
+  });
+}
+
+const left = function(){
+  mainSliderUl.find('li:last-child').insertBefore(mainSliderUl.find('li:first-child'));
+  mainSliderUl.css({'margin-left':-mainImgWidth})
+  mainSliderUl.animate({'margin-left':0},300); 
+}
+
+$('.next_main').click(right);
+$('.prev_main').click(left);
+$('.stop_main').click(function(){
+  clearInterval(intervalId)
+  $('.stop_main > img').hide();
+  $('.play_main > img').show();
+})
+$('.play_main').click(function(){
+  intervalId =  setInterval(right,1000)
+  $('.stop_main > img').show();
+  $('.play_main > img').hide();
+})
+})
+
+/* event_img banner */
+$(function(){
+  const imgBannerWidth = $('.event_img img').width();
+  const imgBannerNember = $('.event_img img').length;
+  const BannerSliderUl = $('.event_img > ul');
+
+  BannerSliderUl.css({width:imgBannerWidth*imgBannerNember})
+
+
+  setInterval(function(){
+    BannerSliderUl.animate({'margin-left':-imgBannerWidth},300, 'swing', function(){
+      BannerSliderUl.css({'margin-left':0})
+      BannerSliderUl.find('li:first-child').insertAfter(BannerSliderUl.find('li:last-child')); 
+      });
+    },3000)
+})
