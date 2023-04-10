aa093a740fce8ad1e5afc619b0fe99d3b4d7ca86
oneme <128768823+RuneRain@users.noreply.github.com>
2023-04-10 12:58:39 +0900

    Add files via upload

---
+++ sulbing/mobile/js/main.js
@@ -0,0 +1,46 @@
+const gnbBtn = document.querySelector('.gnbBtn');
+const gnb_open = document.querySelector('.gnb_open');
+const dark = document.querySelector('.dark');
+
+const btn_subs = document.querySelectorAll('.btn_sub');
+const btn_subArrows = document.querySelectorAll('.ico_arrow_down');
+const list_sub_wraps = document.querySelectorAll('.gnb_sub');
+
+//gnb가 왼쪽에서 튀어나옴
+gnbBtn.addEventListener('click',()=>{
+  gnb_open.style= `transform:translateX(0)`;
+  dark.style.display = 'block';
+})
+
+//gnb가 왼쪽으로 들어감
+dark.addEventListener('click',()=>{
+  gnb_open.style= `transform:translateX(-100%)`;
+  dark.style.display = 'none';
+  loginPop.style.display = 'none';
+})
+
+//gnb내부의 서브(여러개)
+btn_subs.forEach(function(item,index){
+  item.addEventListener('click',function(){
+    btn_subArrows[index].classList.toggle('on')
+    if(btn_subArrows[index].classList.contains('on')){
+      list_sub_wraps[index].style.height = 'auto';
+    } else {
+      list_sub_wraps[index].style.height = '0';
+    }
+  })
+})
+
+/* main slider */
+  const mainImgWidth = $('.slider img').width();
+  const mainImgNember = $('.slider img').length;
+  const mainSliderUl = $('.slider > ul');
+
+  mainSliderUl.css({width:mainImgWidth*mainImgNember})
+
+  setInterval(function(){
+    mainSliderUl.animate({'margin-left':-mainImgWidth},300, 'swing', function(){
+      mainSliderUl.css({'margin-left':0})
+      mainSliderUl.find('li:first-child').insertAfter(mainSliderUl.find('li:last-child')); 
+    });
+  },3000)
