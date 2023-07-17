782dc524a5bec7099ab16ea5882d318a9ac4f395
oneme <128768823+RuneRain@users.noreply.github.com>
2023-07-17 14:35:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,62 @@
 // 스크롤 되면 헤더 모양이 바뀜
-const header = document.querySelector("header");
+const header = document.querySelector(".header");
+const sticker = document.querySelector(".sticker");
+const rect = header.getBoundingClientRect();
+
+const headerTop = rect.bottom;
 
 window.addEventListener("scroll", function () {
-  header.classList.add("on");
-  if (window.scrollY == 0) {
-    header.classList.remove("on");
+  let scroll = window.scrollY;
+  if (headerTop < scroll) {
+    sticker.style = `transform:translateY(0)`;
+  } else {
+    sticker.style = `transform:translateY(-50px)`;
   }
 });
 
+//클릭하면 about이 튀어나옴.
+const aboutMe = document.querySelector('.about_me')
+const profile = document.querySelector('.profile_wrap')
+
+aboutMe.addEventListener('click', (e)=>{
+  profile.style.display='block';
+})
+profile.addEventListener('click',(e)=>{
+  profile.style.display='none';
+})
+
 
 //libray filter
+const projectTagBtn = document.querySelectorAll(".project_nav > button");
+const projectBoxs = document.querySelectorAll(".project_box");
 
-const libraryTagBtn = document.querySelectorAll(".library_nav > button");
-const libraryBoxs = document.querySelectorAll(".library_box");
-
-libraryTagBtn.forEach((button) => {
+projectTagBtn.forEach((button) => {
   button.addEventListener("click", (e) => {
     e.preventDefault();
     const filter = e.target.dataset.filter;
 
-    libraryBoxs.forEach((item) => {
+    projectBoxs.forEach((item) => {
       if (filter === "all") {
-        item.style.display = "block";
+        item.style.display = "flex";
       } else {
         if (item.classList.contains(filter)) {
-          item.style.display = "block";
+          item.style.display = "flex";
         } else {
           item.style.display = "none";
         }
       }
     });
+  });
+});
+
+//animation script
+$(window).scroll(function(){
+  var sct = $(this).scrollTop();
+
+  $('section').each(function(){
+    var sectionOST = $(this).offset().top - 500;
+    if(sct >= sectionOST){
+      $(this).addClass('animate__animated');
+    }
   });
 });
